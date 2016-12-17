204ab861fa9db1d187d11b2652417475b42c4375
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-12-17 12:13:21 +0100

    Add test cases for upper, lower and ucFirst methods

--- test/string-class-test.js
+++ test/string-class-test.js
@@ -14,26 +14,72 @@ describe("hasVowels", () => {
     assert.isFalse('vc'.hasVowels(), "message");
   });
   it('should return false if string has no vowel', () => {
-    assert.isFalse('...£$")()"...'.hasVowels();
+    assert.isFalse('...£$...'.hasVowels());
   });
   it('should return false if string has no vowel', () => {
     assert.isFalse('99999999999'.hasVowels());
   });
 });
-describe("hasVowels", () => {
-  it('should return type of boolean', () => {
-    assert(typeof(('olawale'.hasVowels())) === 'boolean');
+
+describe("toUpper", () => {
+  it('should return type of string', () => {
+    assert(typeof(('olawale'.toUpper())) === 'string');
   });
-  it('should return true if string has vowel', () => {
-    assert.isTrue('olawale'.hasVowels());
+  it('should return upper case for all string', () => {
+    assert(('olawale'.toUpper()) === 'OLAWALE');
   });
-  it('should return false if string has no vowel', () => {
-    assert.isFalse('vc'.hasVowels(), "message");
+  it('should return upper case for all string', () => {
+    assert(('OLAWALE'.toUpper()) === 'OLAWALE');
   });
-  it('should return false if string has no vowel', () => {
-    assert.isFalse('...£$")()"...'.hasVowels();
+  it('should return upper case for all string', () => {
+    assert(('12333'.toUpper()) === '12333');
   });
-  it('should return false if string has no vowel', () => {
-    assert.isFalse('99999999999'.hasVowels());
+  it('should return upper case for all string', () => {
+    assert(('oLaWaLe'.toUpper()) === 'OLAWALE');
+  });
+  it('should return upper case for all string', () => {
+    assert((';.er'.toUpper()) === ';.ER');
+  });
+});
+
+describe("toLower", () => {
+  it('should return type of string', () => {
+    assert(typeof(('olawale'.toLower())) === 'string');
+  });
+  it('should return lower case for all string', () => {
+    assert(('olawale'.toLower()) === 'olawale');
+  });
+  it('should return lower case for all string', () => {
+    assert(('OLAWALE'.toLower()) === 'olawale');
+  });
+  it('should return lower case for all string', () => {
+    assert(('12333'.toLower()) === '12333');
+  });
+  it('should return lower case for all string', () => {
+    assert(('oLaWaLe'.toLower()) === 'olawale');
+  });
+  it('should return lower case for all string', () => {
+    assert((';.ER'.toLower()) === ';.er');
+  });
+});
+
+describe("ucFirst", () => {
+  it('should return type of string', () => {
+    assert(typeof(('olawale'.ucFirst())) === 'string');
+  });
+  it('should return first letter to be Upper case for all string', () => {
+    assert(('olawale'.ucFirst()) === 'Olawale');
+  });
+  it('should return first letter to be Upper case for all string', () => {
+    assert(('OLAWALE'.ucFirst()) === 'OLAWALE');
+  });
+  it('should return first letter to be Upper case for all string', () => {
+    assert(('12333'.ucFirst()) === '12333');
+  });
+  it('should return first letter to be Upper case for all string', () => {
+    assert(('oLaWaLe'.ucFirst()) === 'OLaWaLe');
+  });
+  it('should return first letter to be Upper case for all string', () => {
+    assert((';.ER'.ucFirst()) === ';.ER');
   });
 });
