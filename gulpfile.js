d41a89e79064b9ebb793213f4d42801373f4d8ef
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-01-03 09:59:07 +0100

    Add heroku deployment file

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+/**
+ * Setup browserSync to our server directory,
+ *  which our directory in this case is "src"
+ */
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+      port: process.env.PORT || 5000,
+      open: false
+  });
+});
+
+/**
+ * Add watch function to be able to reload our browser
+ *  whenever changes are been made to js, html, and css files
+ */
+gulp.task('watch', ['browserSync'], function (){
+  gulp.watch(['src/css/**/*.css','src/*.html','src/js/**/*.js'],browserSync.reload); 
+
+});
+
+gulp.task('default', ['watch']);
